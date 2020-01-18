6f8aa6c7806ad268438b4e4651a74d3010fe4324
no-ob <15161159+NO-ob@users.noreply.github.com>
2020-01-18 16:02:11 +0000

    Added Image Saving, Fixed loadtimes with background load property, made it so tags get added to search when clicked in the image window, fixed image previews overlapping, fixed scrolloading

--- src/com/no/loliSnatcher/SearchController.java
+++ src/com/no/loliSnatcher/SearchController.java
@@ -7,17 +7,20 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -31,44 +34,50 @@ public class SearchController {
     @FXML
     private Text output;
     @FXML
-    private Model model = new Model();
+    private Model model = new Model(this);
     @FXML
     private ScrollPane imagePreviews;
     @FXML
     private GridPane imageGrid;
+    private Stage stage;
     int imgCount = 0;
     int rowNum = 0;
     int loading = 0;
+    int colNum = 0;
+    int colMax = 1;
+    String prevTags = "";
     @FXML
     private void processSearch(ActionEvent event){
 
         if (!searchField.getText().isEmpty()){
             imageGrid.getChildren().clear();
+            imagePreviews.setVvalue(0);
+            if (!prevTags.equals(searchField)){
+                colNum = 0;
+            }
             ArrayList<BooruItem> fetched = model.search(searchField.getText());
-            System.out.println(fetched.get(1).tags);
+            System.out.println(fetched.size());
+             if (fetched.size() > 0) {
+                 System.out.println(fetched.get(0).tags);
 
-            rowNum = 0;
-            imgCount = 0;
-            displayImagePreviews(fetched);
-
+                 rowNum = 0;
+                 imgCount = 0;
+                 displayImagePreviews(fetched);
+            }
         }
 
     }
     private void scrollLoad() {
         ArrayList<BooruItem> fetched = model.getNextPage(searchField.getText());
         displayImagePreviews(fetched);
-        PauseTransition pause = new PauseTransition(Duration.seconds(5));
     }
 
 
     private void displayImagePreviews(ArrayList<BooruItem> fetched){
-        int colNum = 0;
-        loading = 1;
+
         while (imgCount < fetched.size()){
             if (colNum > 3){rowNum++;colNum = 0;}
-            ImageView image1 = new ImageView(fetched.get(imgCount).thumbnailURL);
-            image1.setFitWidth((imagePreviews.getLayoutBounds().getWidth() / 4) *0.9);
-            image1.setPreserveRatio(true);
+            ImageView image1 = new ImageView(new Image(fetched.get(imgCount).sampleURL,((imagePreviews.getLayoutBounds().getWidth() / 4) *0.9),0,true,false,true));
             image1.setId("img_"+imgCount);
             image1.setOnMouseClicked(new EventHandler<MouseEvent>() {
                                          @Override public void handle(MouseEvent event) {
@@ -85,10 +94,11 @@ public class SearchController {
             imgCount ++;
             colNum ++;
         }
-        loading = 0;
         imagePreviews.vvalueProperty().addListener(
                 (ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-                    if(newValue.doubleValue() == 1 && loading==0){
+                    System.out.println(oldValue.intValue() + " " +newValue.intValue());
+                    if(newValue.intValue() == 1 && oldValue.intValue()!= 1){
+                        //imagePreviews.setVvalue((double)imgCount / rowNum);
                         System.out.println("hit bottom");
                         scrollLoad();
 
@@ -100,5 +110,10 @@ public class SearchController {
         int id =  Integer.parseInt(imgID);
         model.imageWindowLoader(id);
     }
-
+    public void setStage(Stage thisstage) {
+        stage = thisstage;
+    }
+    public void putTag(String tag){
+        searchField.appendText(" "+tag);
+    }
 }
