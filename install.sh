277a70bdd312339375f3e66613ca9537eb676295
NO_ob <15161159+NO-ob@users.noreply.github.com>
2020-02-05 22:34:59 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -1,36 +1,20 @@
 #!/bin/sh
-### Check we have all programs ###
-for I in wget unzip java awk; do
-  if ! command -v "$I" 1>/dev/null; then
-    echo "Need '$I' command :(" >&2; exit 1
-  fi
-done
-### Check Java version ###
-JAVA_VER=$(java -version 2>&1| awk -F '"' '/version/ {print $2}')
-if [ "$(expr "$JAVA_VER" \< "11.0")" = 1 ]; then
-  echo "Need at least Java 11 :(" >&2; exit 1
-fi
+
 LSDIR="$HOME/.loliSnatcher/"
 mkdir "$LSDIR"
-### Get JavaFX if it is not already in program dir ###
-if [ ! -d "$LSDIR/javafx-sdk-11.0.2" ]; then
-	wget https://download2.gluonhq.com/openjfx/11.0.2/openjfx-11.0.2_linux-x64_bin-sdk.zip
-	unzip openjfx-11.0.2_linux-x64_bin-sdk.zip -d "$LSDIR"
-	rm openjfx-11.0.2_linux-x64_bin-sdk.zip
-fi
-PATH_TO_FX="$LSDIR"javafx-sdk-11.0.2/lib
 
 ### Check for config Directory ###
 if [ ! -d "$LSDIR/config" ]; then
 	mkdir "$LSDIR/config"
 fi
-### Get Program ###
-rm "$LSDIR"LoliSnatcher.jar
-wget https://github.com/NO-ob/LoliSnatcher/releases/download/1.2/LoliSnatcher.jar -P "$LSDIR"
+
 ### Get Icon ###
 if [ ! -f "$LSDIR/icon.png" ]; then
 	wget https://github.com/NO-ob/LoliSnatcher/releases/download/v1.0/icon.png -P "$LSDIR"
 fi
+### Get Binary ###
+wget https://github.com/NO-ob/LoliSnatcher/releases/download/1.3/lolisnatcher -P "$LSDIR"
+chmod u+x "$LSDIR/lolisnatcher"
 ### Get booru configs files ###
 wget https://github.com/NO-ob/LoliSnatcher/releases/download/1.2/booru.zip
 unzip booru.zip -d "$LSDIR"/config
@@ -39,8 +23,8 @@ rm booru.zip
 cat << EOF > ~/.local/share/applications/loliSnatcher.desktop
 [Desktop Entry]
 Name=Loli Snatcher
-StartupWMClass=com.no.loliSnatcher.Main
-Exec=env java -jar --module-path "$PATH_TO_FX" --add-modules javafx.controls,javafx.fxml,javafx.swing "$LSDIR/LoliSnatcher.jar"
+StartupWMClass=loliSnatcher.Main
+Exec="$LSDIR/lolisnatcher"
 Icon=$LSDIR/icon.png
 Type=Application
 Categories=Graphics
